19ad8845c56751358f1946f3bf9d9f401eea4570
Ar-06 <fabiancrack2@gmail.com>
2025-12-16 22:25:07 -0500

    update: add url front on production

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,6 +1,9 @@
 import cors from "cors";
 
-const ACCEPTED_ORIGIN = ["http://localhost:5173"];
+const ACCEPTED_ORIGIN = [
+  "http://localhost:5173",
+  "https://edu-stack-frontend-silk.vercel.app",
+];
 
 export const corsMiddleware = ({ acceptedOrigin = ACCEPTED_ORIGIN } = {}) => {
   return cors({
